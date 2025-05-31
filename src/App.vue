1289a044a0fe33074e9afe81db6a54c56d7a6718
SongYibao <songybmail@foxmail.com>
2025-05-31 21:46:20 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import BasicLayout from '@/Layouts/BasicLayout.vue'
+</script>
+
+<template>
+<!--  <header>-->
+<!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
+
+<!--    <div class="wrapper">-->
+<!--      <HelloWorld msg="You did it!" />-->
+
+<!--      <nav>-->
+<!--        <RouterLink to="/">Home</RouterLink>-->
+<!--        <RouterLink to="/about">About</RouterLink>-->
+<!--      </nav>-->
+<!--    </div>-->
+<!--  </header>-->
+  <BasicLayout></BasicLayout>
+</template>
+
+<style scoped>
+</style>
